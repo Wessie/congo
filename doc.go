128269058d0d0041a5d9a160b7da85cc48d3933f
wessie <github@wessie.info>
2014-06-05 01:01:21 +0200

    More revisions to the package documentation.

--- doc.go
+++ doc.go
@@ -36,7 +36,7 @@ struct that can hold the configuration. So lets do that.
 	//
 	// use as c.Database.Host, c.Database.Port, etc...
 
-Issues
+Issues with traditional
 
 This approach works, and is fairly simple to understand. But there are some
 issues with it.
@@ -45,15 +45,16 @@ issues with it.
 		program. The database config above is only needed by your sql.DB setup.
 	2. Handling default values in your configuration quickly gets ugly.
 	3. If you want to write the config back to file you need to remove default values again,
-		because you don't want all your default values in the file.
+		because you don't want all your default values in the file. This is mostly
+		fixed behind the scenes with congo.
 
-Basic Congo
+The congo way
 
-Congo handles the points above for you. You can re-use existing types except this time
+Congo handles the points above for you. We can reuse our existing types, except this time
 they don't have to be in the same package (or file) or even know about each others existance.
 
-	// we continue with the types above, except we change the Config type to
-	// since it now doesn't need to act as a root anymore.
+	// we continue with the types above, except we can change the Config type
+	// to something more local.
 	type ServerConf struct {
 		Address string
 	}
@@ -63,12 +64,12 @@ they don't have to be in the same package (or file) or even know about each othe
 
 	func init() {
 		// Here we add a child to the congo.Default tree, you can
-		// use your own created congo.Config if you wish to.
+		// use your own created congo.Config if you so desire.
 		//
-		// We name the child "db" as that is the field name used
+		// We name the child "DB" as that is the field name used
 		// in the JSON, and use the databaseConf variable as
-		// target for this field.
-		congo.AddSub("db", &databaseConf)
+		// target for this field just as before.
+		congo.AddSub("DB", &databaseConf)
 	}
 
 	// server.go, some arbitrary file that handles your http server setup
@@ -89,15 +90,13 @@ from a single file. To load a configuration we can do the following in our main.
 		}
 	}
 
-This will have populated both serverConf and databaseConf correctly. This handles
-issue #1 very explicitly, as a bonus congo gave you #3 in the background already,
-even though we have no default handling yet.
+This will populate both serverConf and databaseConf, returning any errors
+that occurred. This handles issue 1 very explicitly (and cleanly).
 
-Defaults
+Handling defaults
 
-Congo uses interface Defaulter to handle defaults. If a type is added as a root or
-child and implements Defaulter, the Default method will be called before unmarshalling
-occurs.
+Congo uses interface Defaulter to handle defaults. If a type handled by congo implements
+Defaulter, the Default will be called before unmarshalling into it.
 
 	func (s *ServerConf) Default() {
 		s.Address = "localhost:8050"
@@ -105,22 +104,25 @@ occurs.
 
 And that's all that is needed. It is critical that Default methods are deterministic,
 because congo will call Default multiple times throughout the parsing process. This gives
-you an easy fix to issue 2, and aids in the solution of issue 3 because we now have
-a deterministic way of getting default values we can compare to when marshalling.
+you an simple to understand, and local fix to issue 2.
 
-Issues with Congo
+We haven't mentioned issue 3 yet, because said issue has already been fixed simply by
+having default values set. Congo will do the rest behind the scenes to avoid writing
+back values that are equal to their defaults.
 
-Congo isn't perfect either though, there are various issues with the current implementation.
+Issues with congo
+
+Congo isn't perfect either, there are various issues with the current implementation.
 
 	1. Only JSON objects are allowed to be roots, especially the exclusion of arrays
-		is something painful for some use cases. This can be fixed though!
-	2. Slow-ish, underneath there are a lot of round-trips between JSON and Go, this
-		might be fixable.
+		is something painful for some use cases. This could be fixed in a future
+		version of congo, but is currently not high on the priority list.
+	2. Slow-ish, underneath there are a lot of round-trips between JSON and Go. This
+		isn't a big issue because you generally don't load many configuration
+		files in a programs lifetime.
 	3. Concurrent-safety, congo currently does not try to be safe when concurrently
 		accessed. Suggestions on how much safety is needed is welcome.
-	3.1 This also means reloading configurations is currently not-safe even though
-		you can reload by repeating the loading step.
-
-See method and function documentation for more info on each.
+	4. With 3 this means reloading configurations is currently not-safe even though
+		you can (unsafely) reload by repeating the loading step.
 */
 package congo
